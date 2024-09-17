065429d581f80efc1cf224fe1bd0ffbfa291c14a
DongHoonYu96 <sbna154@gmail.com>
2024-09-18 00:25:38 +0900

    feat : enum => 값 제한
    role은 user, admin만 가능함

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,16 +1,35 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { RolesEnum } from "../const/roles.const";
 
 @Entity()
 export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  /**
+   * 길이가 20 이하
+   * 유니크 값
+   */
+  @Column({
+    length: 20,
+    unique: true,
+  })
   nickname: string;
 
-  @Column()
+  /**
+   * 유니크 값
+   */
+  @Column({
+    unique: true,
+  })
   email: string;
 
   @Column()
   password: string;
+
+  @Column({
+    enum: Object.values(RolesEnum), //객체의 값들을 가져옴
+    default: RolesEnum.USER, //기본값
+  })
+  role: RolesEnum;
 }
