09f03f210bcacf98abd3c4d101344388226129cb
DongHoonYu96 <sbna154@gmail.com>
2024-09-18 00:19:07 +0900

    feat : user table 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostsModule } from './posts/posts.module';
 import { PostsModel } from "./posts/entities/posts.entity";
+import { UsersModel } from "./users/entities/users.entity";
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { PostsModel } from "./posts/entities/posts.entity";
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',
-      entities: [PostsModel], //생성할 모델들
+      entities: [PostsModel,UsersModel], //생성할 모델들
       synchronize: true, //typeorm 코드와 db싱크를 맞출건지 (개발에서만 써라)
       logging: true, // 모든 쿼리 로깅
       logger: 'advanced-console',
