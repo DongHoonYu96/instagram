43cd8ea33b4fbf00ab85d10226359719eaf82261
DongHoonYu96 <sbna154@gmail.com>
2024-09-18 12:21:08 +0900

    feat : loginWithEmail 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -84,4 +84,11 @@ export class AuthService {
 
     return existingUser;
   }
+
+  async loginWithEmail(user: Pick<UsersModel, 'email' | 'password'>) {
+    const existingUser = await this.authenticationWithEmailAndPassword(user);
+
+    return this.loginUser(existingUser);
+
+  }
 }
