5d49d8b65dd001c80f9340e363612b227484daca
DongHoonYu96 <sbna154@gmail.com>
2024-09-20 11:11:06 +0900

    feat : password 글자수 상한 제한

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -6,6 +6,9 @@ export class PasswordPipe implements PipeTransform {
     if(value.toString().length < 8){
       throw new BadRequestException('비밀번호는 8자 이상이어야 합니다.');
     }
+    if(value.toString().length > 20){
+      throw new BadRequestException('비밀번호는 20자 이하여야 합니다.');
+    }
     return value.toString();
   }
 }
