f882ac211c0e345f309e63868475e9b7142bf72d
DongHoonYu96 <sbna154@gmail.com>
2024-09-21 23:00:50 +0900

    feat: base-pagedto 생성
    pagenation을 공통으로 구현

---
+++ src/common/dto/base-pagination.dto.ts
@@ -0,0 +1,31 @@
+import { IsIn, IsNumber, IsOptional } from "class-validator";
+
+export class BasePaginatePostDto {
+
+  /**
+   * 이전 마지막 데이터의 ID
+   * 이것 이후로 데이터를 가져와야함
+   * ASC와 짝궁(같이 사용해야함)
+   */
+  @IsNumber()
+  @IsOptional()
+  where__id_more_than?:number;
+
+  /**
+   * DESC와 짝궁
+   */
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?:number;
+
+  @IsIn(['ASC', 'DESC']) //값제한
+  @IsOptional()
+  order__createdAt?: 'ASC' = 'ASC' ; //기본값은 ASC
+
+  /**
+   * 몇개의 데이터를 가져올건지
+   */
+  @IsNumber()
+  @IsOptional()
+  take: number=20;
+}
--- src/posts/dto/paginatePostDto.ts
+++ src/posts/dto/paginatePostDto.ts
@@ -1,32 +1,4 @@
-import { IsIn, IsNumber, IsOptional } from "class-validator";
-import { Type } from "class-transformer";
+import { BasePaginatePostDto } from "../../common/dto/base-pagination.dto";
 
-export class PaginatePostDto {
-
-  /**
-   * 이전 마지막 데이터의 ID
-   * 이것 이후로 데이터를 가져와야함
-   * ASC와 짝궁(같이 사용해야함)
-   */
-  @IsNumber()
-  @IsOptional()
-  where__id_more_than?:number;
-
-  /**
-   * DESC와 짝궁
-   */
-  @IsNumber()
-  @IsOptional()
-  where__id_less_than?:number;
-
-  @IsIn(['ASC', 'DESC']) //값제한
-  @IsOptional()
-  order__createdAt?: 'ASC' = 'ASC' ; //기본값은 ASC
-
-  /**
-   * 몇개의 데이터를 가져올건지
-   */
-  @IsNumber()
-  @IsOptional()
-  take: number=20;
+export class PaginatePostDto extends BasePaginatePostDto{
 }
