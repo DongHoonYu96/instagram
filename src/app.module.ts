f8a14fdb1b0458835feaab745a7506b7d61a8859
DongHoonYu96 <sbna154@gmail.com>
2024-09-22 16:40:02 +0900

    feat : 정적파일제공 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,21 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY
 } from "./common/const/env-keys.const";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { PUBLIC_FOLDER_PATH } from "./common/const/path.const";
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
+    ServeStaticModule.forRoot({
+      //이 경로에 있는 이미지들을 제공해줄것임.
+      rootPath: PUBLIC_FOLDER_PATH, //public 폴더의 절대경로 : D:nest/insta/public
+      //문제 : root인 public폴더는 제외된채로 외부에 전달됨 -> 기존의 컨트롤러 url과 겹침 (/posts/4022.jpg)
+      //해결 : serveRoot 추가(/public/posts/4022.jpg)
+
+      serveRoot: '/public',
+    }),
     ConfigModule.forRoot({
       envFilePath: ".env",
       isGlobal: true, //모든서비스에 자동 import
